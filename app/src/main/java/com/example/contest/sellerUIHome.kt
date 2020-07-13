b01ce944b11aba741718821b311640a99f4177bf
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2020-07-14 00:12:05 +0900

    .
    .

--- app/src/main/java/com/example/contest/sellerUIHome.kt
+++ app/src/main/java/com/example/contest/sellerUIHome.kt
@@ -11,16 +11,6 @@ class sellerUIHome : Fragment() {
     override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.seller_ui_home, container, false)
 
-<<<<<<< HEAD
-        enrollProduct.setOnClickListener{
-            val sellerEnroll = Intent(getActivity(),sellerUIEnrollment::class.java)
-            startActivity(sellerEnroll)
-        }
-=======
-
-
-
         return view
->>>>>>> 8d729a4ea0b87e96c77fb081fe93571b8e97668a
     }
 }
