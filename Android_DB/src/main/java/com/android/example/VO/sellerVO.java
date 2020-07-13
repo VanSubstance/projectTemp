434229d74a1bbc85e388562560521d86b784f4eb
Kim-Jong-Gyu <62927374+Kim-Jong-Gyu@users.noreply.github.com>
2020-07-13 16:29:48 +0900

    험난한 스프링 연결 2

---
+++ Android_DB/src/main/java/com/android/example/VO/sellerVO.java
@@ -0,0 +1,19 @@
+package com.android.example.VO;
+
+public class sellerVO {
+	private int marketID;
+	private String sellerID;
+	private String sellerPW;
+	private String address;
+	private String category;
+	private String time;
+	private int cell_phone;
+	
+	public int getMarketID() {
+		return marketID;
+	}
+	public void setMarketID() {
+		this.marketID=marketID;
+	}
+	
+}
