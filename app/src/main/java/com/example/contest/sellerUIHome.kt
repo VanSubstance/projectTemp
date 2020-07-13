11cf9535050f4ec0d89910ebeb6c2679cb40c938
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2020-07-13 18:48:16 +0900

    Revert "Merge branch 'master' of https://github.com/VanSubstance/projectTemp"
    This reverts commit c73a4a8b860fa74e7c04ea70a395640d57764b6d, reversing
    changes made to 37f0687328ccffbc5698ddd5c01d8276b636819c.

--- app/src/main/java/com/example/contest/sellerUIHome.kt
+++ app/src/main/java/com/example/contest/sellerUIHome.kt
@@ -1,18 +1,19 @@
 package com.example.contest
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.seller_ui_home.*
 
 class sellerUIHome : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.seller_ui_home, container, false)
-        return view
 
+
+
+
+        return view
     }
 }
