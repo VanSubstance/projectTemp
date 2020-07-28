78acb7ed66fa295a46c9cc01cebd51d734f23514
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2020-07-29 05:32:06 +0900

    시장 위치 수정
    - 시장 위치 마킹 틀 구현

--- app/src/main/java/com/example/contest/buyerUIToday.kt
+++ app/src/main/java/com/example/contest/buyerUIToday.kt
@@ -1,14 +1,18 @@
 package com.example.contest
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.UiSettings
+import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.util.FusedLocationSource
 
 
@@ -44,19 +48,21 @@ class buyerUIToday : Fragment(), OnMapReadyCallback {
         return view
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onMapReady(naverMap: NaverMap) {
         locationSource = FusedLocationSource(this, ACCESS_LOCATION_PERMISSION_REQUEST_CODE)
         naverMap.setLocationSource(locationSource);
         val uiSettings: UiSettings = naverMap.uiSettings
         uiSettings.setLocationButtonEnabled(true)
-
-
-        /*
-        val marker = Marker()
-        marker.position = LatLng(37.359184, 127.104832)
-        marker.map = naverMap
+        /**
+        var markerList : Array<Marker> = arrayOf()
+        var coordinateList = readDate()
+        for ( i in coordinateList) {
+            var marker = Marker()
+            marker.position = LatLng(i[1] as Double, i[2] as Double)
+            marker.map = naverMap
+        }
         */
-        
 
     }
 
@@ -72,6 +78,15 @@ class buyerUIToday : Fragment(), OnMapReadyCallback {
                 return
             }
         }
+    }
+
+    // 시장 정보(이름, 위도, 경도) 불러오기
+    // 시장 데이터: https://discordapp.com/channels/728900415638208564/728900416086999082/737768095610961990
+    // 위에 시장 데이터를 DB에 삽입
+    // DB에서 값 불러오기 (이름, 위도, 경도)
+    fun readDate(): Array<Array<Any>> {
+        var container : Array<Array<Any>> = arrayOf()
+        return container;
     }
 
 }
