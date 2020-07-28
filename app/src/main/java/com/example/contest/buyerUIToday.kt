50ca3a8fe7fa54522eb015b887f1ee29828019fe
newnewnewnewbie <67823201+newnewnewnewbie@users.noreply.github.com>
2020-07-28 19:54:39 +0900

    구매자 지도UI GPS 구현
    GPS를 받아 구매자 현재 위치로 카메라 이동

--- app/src/main/java/com/example/contest/buyerUIToday.kt
+++ app/src/main/java/com/example/contest/buyerUIToday.kt
@@ -5,20 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
-import com.naver.maps.map.overlay.LocationOverlay
-import com.naver.maps.map.overlay.Marker
-import kotlinx.android.synthetic.main.buyer_ui_today.*
+import com.naver.maps.map.UiSettings
+import com.naver.maps.map.util.FusedLocationSource
+
 
 class buyerUIToday : Fragment(), OnMapReadyCallback {
 
+    private val ACCESS_LOCATION_PERMISSION_REQUEST_CODE = 100
+    private var locationSource: FusedLocationSource? = null
+
+    /*
     private lateinit var productElementList: ArrayList<productElement>
     private val linearLayoutManager by lazy { LinearLayoutManager(context) }
     private lateinit var adapter: productElementAdapter
+    */
 
     companion object {
         fun newInstance(): buyerUIToday {
@@ -42,18 +45,33 @@ class buyerUIToday : Fragment(), OnMapReadyCallback {
     }
 
     override fun onMapReady(naverMap: NaverMap) {
+        locationSource = FusedLocationSource(this, ACCESS_LOCATION_PERMISSION_REQUEST_CODE)
+        naverMap.setLocationSource(locationSource);
+        val uiSettings: UiSettings = naverMap.uiSettings
+        uiSettings.setLocationButtonEnabled(true)
 
 
+        /*
         val marker = Marker()
         marker.position = LatLng(37.359184, 127.104832)
         marker.map = naverMap
+        */
+        
 
-/*
-        val locationOverlay = naverMap.locationOverlay
-        locationOverlay.isVisible = true
+    }
 
-        locationOverlay.position = LatLng(37.359184, 127.104832)
-*/
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            ACCESS_LOCATION_PERMISSION_REQUEST_CODE -> {
+                locationSource!!.onRequestPermissionsResult(requestCode, permissions, grantResults)
+                return
+            }
+        }
     }
 
 
