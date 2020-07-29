2f69363b82a971e875f62cee221213fb4aa1693f
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2020-07-29 16:05:46 +0900

    ?
    ?

--- app/src/main/java/com/example/contest/buyerUIRecipe.kt
+++ app/src/main/java/com/example/contest/buyerUIRecipe.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.buyer_ui_recipe.*
 import kotlinx.android.synthetic.main.buyer_ui_recipe.view.*
 
 class buyerUIRecipe : Fragment() {
@@ -13,15 +14,31 @@ class buyerUIRecipe : Fragment() {
         val view = inflater.inflate(R.layout.buyer_ui_recipe, container, false)
 
         view.buttonRecipe1.setOnClickListener {
+            buttonRecipe1.setImageResource(R.drawable.ui_ctgr_meat)
+            buttonRecipe2.setImageResource(R.drawable.ui_ctgr_seafood_off)
+            buttonRecipe3.setImageResource(R.drawable.ui_ctgr_vegetable_off)
+            buttonRecipe4.setImageResource(R.drawable.ui_ctgr_etc_off)
             (activity as buyerUIMain).setBuyerFrag(12)
         }
         view.buttonRecipe2.setOnClickListener {
+            buttonRecipe1.setImageResource(R.drawable.ui_ctgr_meat_off)
+            buttonRecipe2.setImageResource(R.drawable.ui_ctgr_seafood)
+            buttonRecipe3.setImageResource(R.drawable.ui_ctgr_vegetable_off)
+            buttonRecipe4.setImageResource(R.drawable.ui_ctgr_etc_off)
             (activity as buyerUIMain).setBuyerFrag(13)
         }
         view.buttonRecipe3.setOnClickListener {
+            buttonRecipe1.setImageResource(R.drawable.ui_ctgr_meat_off)
+            buttonRecipe2.setImageResource(R.drawable.ui_ctgr_seafood_off)
+            buttonRecipe3.setImageResource(R.drawable.ui_ctgr_vegetable)
+            buttonRecipe4.setImageResource(R.drawable.ui_ctgr_etc_off)
             (activity as buyerUIMain).setBuyerFrag(14)
         }
         view.buttonRecipe4.setOnClickListener {
+            buttonRecipe1.setImageResource(R.drawable.ui_ctgr_meat_off)
+            buttonRecipe2.setImageResource(R.drawable.ui_ctgr_seafood_off)
+            buttonRecipe3.setImageResource(R.drawable.ui_ctgr_vegetable_off)
+            buttonRecipe4.setImageResource(R.drawable.ui_ctgr_etc)
             (activity as buyerUIMain).setBuyerFrag(15)
         }
 
--- app/src/main/java/com/example/contest/recipeUISpecific.kt
+++ app/src/main/java/com/example/contest/recipeUISpecific.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.recipe_ui_specific.*
+import kotlinx.android.synthetic.main.recipe_ui_element.view.*
 import kotlinx.android.synthetic.main.recipe_ui_specific.view.*
 
 class recipeUISpecific : Fragment() {
@@ -14,7 +14,7 @@ class recipeUISpecific : Fragment() {
         val view = inflater.inflate(R.layout.recipe_ui_specific, container, false)
 
         view.textTitle.text = arguments?.getString("title")
-        view.textCtgr.text = arguments?.getString("ctgrBig")
+        view.recipeCtgr.text = arguments?.getString("ctgrBig")
 
         return view
     }
