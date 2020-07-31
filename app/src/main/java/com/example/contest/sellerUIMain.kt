0edcd59bbf96dcb4bd114b2763b76d60d1724926
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2020-08-01 02:36:28 +0900

    자잘한 기능 수정
    - 로그인 toast 오류 수정
    - 뒤로가기 버튼 1회 -> 각 엑티비티 메인화면으로 이동
    - 뒤로가기 버튼 2회 -> 무조건 종료

--- app/src/main/java/com/example/contest/sellerUIMain.kt
+++ app/src/main/java/com/example/contest/sellerUIMain.kt
@@ -1,13 +1,17 @@
 package com.example.contest
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.seller_ui_main.*
 import kotlinx.android.synthetic.main.seller_ui_main.textTime
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.collections.ArrayList
 
 class sellerUIMain : AppCompatActivity() {
 
@@ -84,7 +88,28 @@ class sellerUIMain : AppCompatActivity() {
             }
         }
     }
+
+    // 뒤로가기 테스트
+    private final var FINISH_INTERVAL_TIME: Long = 2000
+    override fun onBackPressed() {
+        if(supportFragmentManager.backStackEntryCount == 0) {
+            var tempTime = System.currentTimeMillis()
+            var intervalTime = tempTime - backPressedTime.flag
+            if (0 <= intervalTime && FINISH_INTERVAL_TIME >= intervalTime) {
+                finishAffinity()
+                System.exit(0)
+                super.onBackPressed()
+            } else {
+                backPressedTime.flag = tempTime;
+                startActivity(Intent(this, sellerUIMain::class.java))
+                Toast.makeText(this, "'뒤로' 버튼을 한 번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show()
+                return
+            }
+        }
+        super.onBackPressed()
+    }
 }
+
 object currentProductElement {
     var currentProductElement = productElement()
 }
