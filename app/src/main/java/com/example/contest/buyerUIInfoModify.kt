25ab1193d7c909a0f607d33ecc238e82fb9e4dd1
newnewnewnewbie <67823201+newnewnewnewbie@users.noreply.github.com>
2020-08-01 18:19:51 +0900

    구매자 정보수정 ui 변경
    사진 변경과 닉네임 추가

--- app/src/main/java/com/example/contest/buyerUIInfoModify.kt
+++ app/src/main/java/com/example/contest/buyerUIInfoModify.kt
@@ -5,11 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.buyer_ui_info.view.*
 import kotlinx.android.synthetic.main.buyer_ui_info_modify.view.*
+import kotlinx.android.synthetic.main.buyer_ui_info_modify.view.imageUser
+
 class buyerUIInfoModify : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.buyer_ui_info_modify, container, false)
+        view.imageUser.setImageResource(R.drawable.test)
         view.buttonCancel.setOnClickListener {
             (activity as buyerUIMain).setBuyerFrag(31)
         }
