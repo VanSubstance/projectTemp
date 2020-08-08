7d528a2d03c101f86544b7519ca032713b7b2e8b
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2020-08-08 17:34:03 +0900

    Update integrator.py

--- Documents/integrator.py
+++ Documents/integrator.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2dfe5d0679015f2994de1c3bbb0a87668c8336126aef785e05aeecc84b5a8e1b
-size 2326
+oid sha256:8aef8ad9977620937329a2a19b9b6daafa92efaed6814d9856bc903c0cefaecf
+size 2330
