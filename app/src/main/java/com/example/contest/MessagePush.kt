26b7445c5bbfc624368d663a0475068293d49e48
Kim-Jong-Gyu <62927374+Kim-Jong-Gyu@users.noreply.github.com>
2020-08-08 16:09:44 +0900

    PUSH 끝
    ㅎㅎ

--- app/src/main/java/com/example/contest/MessagePush.kt
+++ app/src/main/java/com/example/contest/MessagePush.kt
@@ -1,48 +1,50 @@
 package com.example.contest
 
 
+import android.app.Notification
+import android.os.AsyncTask
 import android.util.Log
-
-import com.google.firebase.database.FirebaseDatabase
-import com.squareup.okhttp.*
+import com.squareup.okhttp.MediaType
+import com.squareup.okhttp.OkHttpClient
+import com.squareup.okhttp.Request
+import com.squareup.okhttp.RequestBody
 import org.json.JSONObject
-import java.io.IOException
-
-import java.lang.Exception
 
 
-class MessagePush() {
-    fun sendMessage(destinationToken: String, title: String, message: String) {
-        try{
-            val database: FirebaseDatabase = FirebaseDatabase.getInstance()
-            val JSON = MediaType.parse("application/json; charset=utf-8")
-            val url = "https://fcm.googleapis.com/fcm/send"
-            val serverKey =
-                "AAAAv115-6g:APA91bHF9UX5qTiV2JmhM5oJrjq8NL6VuYciFPmV3cvyc3Z9qUJtRNE-y3E5aOMqn6e0prefmXEg2riitvF22PMHZywxcQtotCSEMZJEGPyXFwEN9642neblUBtlk492JHeTCG8CIhcO"
 
-            var client: OkHttpClient? = OkHttpClient()
-            var json = JSONObject()
-            var dataJson = JSONObject()
-            dataJson.put("body",message)
-            dataJson.put("title",title)
-            json.put("notification",dataJson)
-            json.put("to",destinationToken)
-            val body=RequestBody.create(JSON,json.toString())
-            val request=Request.Builder()
-                .header("Authorization","key="+serverKey)
-                .url(url)
-                .post(body)
-                .build()
-            val response= client?.newCall(request)?.execute()
-            client?.newCall(request)?.enqueue(object :Callback{
-                override fun onFailure(request: Request?, e: IOException?) {
+class MessagePush {
+    fun sendNotification(
+        regToken: String?,
+        title: String?,
+        messsage: String?
+    ) {
+        object : AsyncTask<Void?, Void?, Void?>() {
+            override fun doInBackground(vararg params: Void?): Void? {
+                try {
+                    val JSON = MediaType.parse("application/json; charset=utf-8")
+                    val serverKey =
+                        "AAAAv115-6g:APA91bHF9UX5qTiV2JmhM5oJrjq8NL6VuYciFPmV3cvyc3Z9qUJtRNE-y3E5aOMqn6e0prefmXEg2riitvF22PMHZywxcQtotCSEMZJEGPyXFwEN9642neblUBtlk492JHeTCG8CIhcO"
+                    val client = OkHttpClient()
+                    val json = JSONObject()
+                    val dataJson = JSONObject()
+                    dataJson.put("body", messsage)
+                    dataJson.put("title", title)
+                    json.put("notification", dataJson)
+                    json.put("to", regToken)
+                    val body =
+                        RequestBody.create(JSON, json.toString())
+                    val request=Request.Builder()
+                        .header("Authorization", "key=" + serverKey)
+                        .url("https://fcm.googleapis.com/fcm/send")
+                        .post(body)
+                        .build()
+                    val response = client.newCall(request).execute()
+                    val finalResponse = response.body().string()
+                } catch (e: Exception) {
+                    Log.d("error", e.toString() + "")
                 }
-                override fun onResponse(response: Response?) {
-                    val finalrespons=response?.body().toString()
-                }
-            })
-        }catch (e:Exception){
-            Log.d("error",e.toString())
-        }
+                return null
+            }
+        }.execute()
     }
 }
