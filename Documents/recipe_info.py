4012f8bd96564ced6702e5987f52bfe5a347cc88
Cho-Joon-Hee <ehdro64@gmail.com>
2020-08-09 23:18:12 +0900

    레시피

--- Documents/recipe_info.py
+++ Documents/recipe_info.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a24f1cbf073202df3b2802444a0c1e96ed3f6e83df215c1c6a965bb0ceabe009
-size 2890
+oid sha256:eef76a93c5bef17a0897474df80ee82f96341f15ab3cc8ad4b02c6534afffc94
+size 2834
