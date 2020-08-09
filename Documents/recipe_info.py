ddae802aa30f5720abb048907f76e92457981a93
Kim-Jong-Gyu <62927374+Kim-Jong-Gyu@users.noreply.github.com>
2020-08-09 23:53:54 +0900

    건들지 마용

--- Documents/recipe_info.py
+++ Documents/recipe_info.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18431062967fe31aadb148b38d34489c62b09861eda7f80cd3feb7c023540141
-size 284
+oid sha256:781a65023a51d2f5bf7235073588ed19b6d0684f54cec8689e1c446aaf829b75
+size 2602
