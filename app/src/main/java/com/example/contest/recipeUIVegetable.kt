fa1a4778abefc9c0c596d84ddd3cb71e6f782bf3
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2020-08-11 00:11:46 +0900

    레시피 리스트 기능 수정
    - 재료 검색 기능 추가
    - 리스트 순서 섞기 추가

--- app/src/main/java/com/example/contest/recipeUIVegetable.kt
+++ app/src/main/java/com/example/contest/recipeUIVegetable.kt
@@ -26,7 +26,43 @@ class recipeUIVegetable : Fragment() {
         val view = inflater.inflate(R.layout.recipe_ui_vegetable,container,false)
 
         view.buttonSearchRecipe.setOnClickListener {
+            RecyclerView.layoutManager = linearLayoutManager
 
+            recipeElementList = ArrayList()
+
+            var ctgrList: ArrayList<String> = arrayListOf()
+            ctgrList.add("건어물")
+            ctgrList.add("해물")
+
+            database.getReference("recipeDB")
+                .addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onCancelled(p0: DatabaseError) {
+                    }
+                    override fun onDataChange(p0: DataSnapshot) {
+                        for (recipeCtgr in p0.children) {
+                            // 큰 카테고리에 포함 될 경우
+                            if (ctgrList.any{it.equals(recipeCtgr.key.toString())}) {
+                                var recipeList: ArrayList<recipeElement> = arrayListOf()
+                                for (recipe in recipeCtgr.children) {
+                                    for (ingre in recipe.child("ingredient")
+                                        .child("재료").value as ArrayList<String>) {
+                                        if (ingre.contains(view!!.textRecipeTitle.text.toString())) {
+                                            var element = recipeElement()
+                                            element.setFromDB(recipe, recipeCtgr.key.toString())
+                                            recipeList.add(element)
+                                            break
+                                        }
+                                    }
+                                }
+                                addList(recipeList, recipeElementList)
+                            }
+                        }
+                        adapter = recipeUIAdapter(recipeElementList, requireContext(), 1) { recipeElement ->
+                            (activity as buyerUIMain).showRecipeSpecific(recipeElement)
+                        }
+                        RecyclerView.adapter = adapter
+                    }
+                })
         }
 
         return view
